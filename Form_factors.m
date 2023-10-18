f16d5c74e305ce47bfc48accc921d3bb4cb713d4
scatterer <148332025+scatterer@users.noreply.github.com>
2023-10-18 13:38:34 +0200

    Add files via upload

--- Form_factors.m
+++ Form_factors.m
@@ -6,11 +6,14 @@
 % To do as user:    Nothing.
 %
 % Note:
+% Copyright (C) 2023 by the authors - All Rights Reserved
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details. A copy of the GNU
+% General Public License can be obtained from the
+% Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 function [f, f_sqrd_real, f_av_sqrd_real] = Form_factors(Q, FF, composition)
 
 % Q dependent form factors
--- Read_form_factor_coefficients.m
+++ Read_form_factor_coefficients.m
@@ -5,10 +5,14 @@
 % To do as user:    Nothing.
 %
 % Note:
+% Copyright (C) 2023 by the author - All Rights Reserved
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details. A copy of the GNU
+% General Public License can be obtained from the
+% Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%:w
 
 function [FF_coeff] = Read_form_factor_coefficients(Z, wavelength)
 
